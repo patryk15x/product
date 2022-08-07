c33c6bb5777cb5f2c1637de5e5faf81ff8bd13cc
Patryk Szaruga <szarugapatryk@gmail.com>
2022-08-07 20:07:53 +0200

    initial commit

---
+++ src/main/groovy/com/micro/product/ProductApplication.groovy
@@ -0,0 +1,13 @@
+package com.micro.product
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProductApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ProductApplication, args)
+    }
+
+}
