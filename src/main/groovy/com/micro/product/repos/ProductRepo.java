1d892316211173cdd426e13e23f62323f3602077
Patryk Szaruga <szarugapatryk@gmail.com>
2022-08-07 20:18:20 +0200

    add Product repo

---
+++ src/main/groovy/com/micro/product/repos/ProductRepo.java
@@ -0,0 +1,7 @@
+package com.micro.product.repos;
+
+import com.micro.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+}
