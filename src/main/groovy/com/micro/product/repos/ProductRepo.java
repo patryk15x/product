826b32e5875b8e5096593c14493748c90d020a17
Patryk Szaruga <szarugapatryk@gmail.com>
2022-08-07 20:31:06 +0200

    add Product repo

--- src/main/groovy/com/micro/product/repos/ProductRepo.java
+++ src/main/groovy/com/micro/product/repos/ProductRepo.java
@@ -4,4 +4,5 @@ import com.micro.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
+    Product findByName(String name);
 }
